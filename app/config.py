dbfded9f6a2137c2d118b77516159428e06c6b3f
kredabtc <36606272+kredabtc@users.noreply.github.com>
2018-02-18 21:52:15 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@
 # Get an Key and Secret 
 # https://www.kucoin.com/#/user/setting/api
 
-api_key = '6cCWQON2THWRF10oGefEXIOwJO50psThICSQU9CFVpE8fPshX11HKIQAiSpZvoZh'
-api_secret = 'XdzFfEinOI5QZU8Fiu0CKzMZU8vUW9gvvDp2oON80wEk7fHPWDulxFpIUws2kavE'
+api_key = ''
+api_secret = ''
 
 recv_window = 6000000
