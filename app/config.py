65adbcab93a24142743880c7bef2812e4e408729
kredabtc <36606272+kredabtc@users.noreply.github.com>
2018-02-18 21:24:27 +0100

    Update and rename config.sample.py to config.py

---
+++ app/config.py
@@ -0,0 +1,9 @@
+# @yasinkuyu
+
+# Get an Key and Secret 
+# https://www.kucoin.com/#/user/setting/api
+
+api_key = '6cCWQON2THWRF10oGefEXIOwJO50psThICSQU9CFVpE8fPshX11HKIQAiSpZvoZh'
+api_secret = 'XdzFfEinOI5QZU8Fiu0CKzMZU8vUW9gvvDp2oON80wEk7fHPWDulxFpIUws2kavE'
+
+recv_window = 6000000
